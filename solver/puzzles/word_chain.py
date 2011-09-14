fec1c5990b8ea6768b062e1e5a9cb006e28f6c01
Orion Auld <orion.auld@gmail.com>
2011-09-13 23:02:01 -0700

    starting to flesh out the domain objects for word chains

--- solver/puzzles/word_chain.py
+++ solver/puzzles/word_chain.py
@@ -4,7 +4,19 @@ from solver.dictionaries import bigram
 
 class WordChain(object):
     def __init__(self, words):
-          self.words = words
+        entries = [[w, {'word': w, 'link': None, 'prev': None, 'next': None}] for w in words]
+        self.chain = dict(entries)
+        self.unlinked_before = set(words)
+        self.unlinked_after = set(words)
+
+    def link(self, a, link_word, b):
+        # TODO: Confirm at least that b exists in the chain
+        self.chain[a]['link'] = link_word 
+        self.chain[a]['next'] = self.chain[b] 
+        self.chain[b]['prev'] = self.chain[a]
+        self.unlinked_before.remove(b)
+        self.unlinked_after.remove(a)
+
 
 
 class WordChainProblem(Problem):
--- test/solver/puzzles/test_word_chain.py
+++ test/solver/puzzles/test_word_chain.py
@@ -1,7 +1,19 @@
 from solver.puzzles.word_chain import * 
 
 class TestWordChain:
-  def test_one(self):
-    words = ["wing", "child"]
-    chain = WordChain(words)
-    assert 1 == 1 
+    def setup_method(self, method):
+        self.words = ["racer", "wing", "child"]
+        self.chain = WordChain(self.words)
+        
+    def test_unlinked_at_start(self):
+        assert self.chain.unlinked_before == set(self.words)
+        assert self.chain.unlinked_after == set(self.words)
+
+    def test_link(self):
+        self.chain.link("racer", "x", "wing")
+
+    def test_unlinked_after_link(self):
+        self.chain.link("racer", "x", "wing")
+        assert "wing" not in self.chain.unlinked_before
+        assert "racer" not in self.chain.unlinked_after
+
